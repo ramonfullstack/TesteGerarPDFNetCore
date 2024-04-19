bcb4a5439432b9779d4d00ea245aafb9e8655195
santosrs1 <Ramon.Santos-consultorexterno@rabobank.com>
2024-04-19 10:55:26 -0300

    Alterado para gerar pdf

--- TesteGeracaoPDF/Controllers/PDFController.cs
+++ TesteGeracaoPDF/Controllers/PDFController.cs
@@ -14,11 +14,6 @@ namespace TesteGeracaoPDF.Controllers
     [Route("[controller]")]
     public class PdfController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<PdfController> _logger;
 
         public PdfController(ILogger<PdfController> logger)
@@ -27,47 +22,54 @@ namespace TesteGeracaoPDF.Controllers
         }
 
         [HttpGet]
-        public string GetPDF()
+        public IActionResult GetPDF()
         {
-            try
-            {
-                Document doc = new Document(PageSize.A4);
-                doc.SetMargins(40, 40, 40, 80);
-                doc.AddCreationDate();
-
-                //caminho onde sera criado o pdf + nome desejado
-                //OBS: o nome sempre deve ser terminado com .pdf
-                string caminho = @"C:\temp\teste.pdf";
-
-                PdfWriter writer = PdfWriter.GetInstance(doc, new
-                FileStream(caminho, FileMode.Create));
-
-                doc.Open();
-
-                //criando uma string vazia
-                string dados = "";
-
-                //criando a variavel para paragrafo
-                Paragraph paragrafo = new Paragraph(dados,
-                new Font(Font.NORMAL, 14));
-                //etipulando o alinhamneto
-                paragrafo.Alignment = Element.ALIGN_JUSTIFIED;
-                //Alinhamento Justificado
-                //adicioando texto
-                paragrafo.Add("TESTE TESTE TESTE");
-                //acidionado paragrafo ao documento
-                doc.Add(paragrafo);
-                //fechando documento para que seja salva as alteraçoes.
-                doc.Close();
-
-                return "Pdf gerado com sucesso";
-            }
-            catch (Exception ex)
-            {
-
-                return "Erro na geração de pdf: " + $"Erro: {ex.GetBaseException().Message}";
-            }
-            
+            var pdfService = new GeracaoPDFService();
+            byte[] pdfBytes = pdfService.GerarPDF();
+            return File(pdfBytes, "application/pdf", "meu_arquivo.pdf");
         }
+
+        //[HttpGet("GetPDFDefault")]
+        //public string GetPDFDefault(string cliente, string data)
+        //{
+        //    try
+        //    {
+        //        Document doc = new Document(PageSize.A4);
+        //        doc.SetMargins(40, 40, 40, 80);
+        //        doc.AddCreationDate();
+
+        //        //caminho onde sera criado o pdf + nome desejado
+        //        //OBS: o nome sempre deve ser terminado com .pdf
+        //        string caminho = @"C:\temp\teste.pdf";
+
+        //        PdfWriter writer = PdfWriter.GetInstance(doc, new
+        //        FileStream(caminho, FileMode.Create));
+        //        doc.Open();
+
+        //        //criando uma string vazia
+        //        string dados = "";
+
+        //        //criando a variavel para paragrafo
+        //        Paragraph paragrafo = new Paragraph(dados,
+        //        new Font(Font.NORMAL, 14));
+               
+        //        paragrafo.Alignment = Element.ALIGN_JUSTIFIED;
+           
+        //        paragrafo.Add("TESTE TESTE TESTE");
+        //        //acidionado paragrafo ao documento
+        //        doc.Add(paragrafo);
+        //        //fechando documento para que seja salva as alteraçoes.
+        //        doc.Close();
+
+        //        return $"PDF GERADO COM SUCESSO EM {caminho}";
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return $"Erro na geração de pdf Erro: {ex.GetBaseException().Message}";
+        //    }
+            
+        //}
+
+      
     }
 }
---
+++ TesteGeracaoPDF/Servicos/GeracaoPDFService.cs
@@ -0,0 +1,29 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+
+namespace TesteGeracaoPDF
+{
+    public class GeracaoPDFService
+    {
+        public byte[] GerarPDF()
+        {
+            // Criar um novo documento PDF
+            Document doc = new Document();
+            MemoryStream ms = new MemoryStream();
+            PdfWriter writer = PdfWriter.GetInstance(doc, ms);
+
+            doc.Open();
+            doc.Add(new Paragraph("Conteúdo do PDF"));
+
+            doc.Close();
+            writer.Close();
+
+            return ms.ToArray();
+        }
+    }
+}
+
+
+
+
