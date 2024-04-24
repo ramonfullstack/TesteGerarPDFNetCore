bac61e6aafcd806ba92baf819863eb960889b960
santosrs1 <Ramon.Santos-consultorexterno@rabobank.com>
2024-04-24 16:47:02 -0300

    método para gerar arquivo a partir de outro pdf

---
+++ TesteGeracaoPDF/Controllers/PDFODSController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace TesteGeracaoPDF.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PDFODSController : ControllerBase
+    {
+        private readonly ILogger<PDFODSController> _logger;
+
+        public PDFODSController(ILogger<PDFODSController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetPDF(string cliente, string data, string caminho = "")
+        {
+            try
+            {
+                var pdfService = new GeracaoPDFService();
+                byte[] pdfBytes;
+                if (string.IsNullOrEmpty(caminho))
+                {
+                    pdfBytes = pdfService.GerarPDFODS(cliente, data);
+                }
+                else
+                {
+                    pdfBytes = pdfService.GerarPDFODSDeArquivo(caminho);
+                }
+
+                return File(pdfBytes, "application/pdf", "meu_arquivo.pdf");
+            }
+            catch (Exception ex)
+            {
+                return NoContent();
+            }
+           
+        }
+    }
+}
--- TesteGeracaoPDF/Servicos/GeracaoPDFService.cs
+++ TesteGeracaoPDF/Servicos/GeracaoPDFService.cs
@@ -1,5 +1,6 @@
 ﻿using iTextSharp.text;
 using iTextSharp.text.pdf;
+using System;
 using System.IO;
 
 namespace TesteGeracaoPDF
@@ -21,6 +22,83 @@ namespace TesteGeracaoPDF
 
             return ms.ToArray();
         }
+
+        public byte[] GerarPDFODS(string cod_cliente, string data)
+        {
+            // Criar um novo documento PDF
+            Document doc = new Document();
+            MemoryStream ms = new MemoryStream();
+            PdfWriter writer = PdfWriter.GetInstance(doc, ms);
+
+            doc.Open();
+            doc.Add(new Paragraph("PDF da ODS"));
+            doc.Add(new Paragraph($"Código cliente: {cod_cliente}"));
+            doc.Add(new Paragraph($"Conteúdo do PDF: {data}"));
+
+            doc.Close();
+            writer.Close();
+
+            return ms.ToArray();
+        }
+
+        public byte[] GerarPDFODSDeArquivo(string caminho_arquivo)
+        {
+            // Verifica se o arquivo existe
+            if (!File.Exists(caminho_arquivo))
+            {
+                throw new FileNotFoundException("O arquivo PDF não foi encontrado.", caminho_arquivo);
+            }
+
+            byte[] pdfBytes;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+               
+                try
+                {
+                    Document doc = new();
+                    PdfWriter writer = PdfWriter.GetInstance(doc, ms);
+                    doc.Open();
+
+                    // Abra o arquivo PDF existente
+                    using (PdfReader reader = new PdfReader(caminho_arquivo))
+                    {
+                        for (int i = 1; i <= reader.NumberOfPages; i++)
+                        {
+                            doc.NewPage();
+                            PdfImportedPage page = writer.GetImportedPage(reader, i);
+                            writer.DirectContent.AddTemplate(page, 0, 0);
+                        }
+                    } 
+                    // O arquivo PDF será fechado automaticamente aqui
+                }
+                catch (Exception ex)
+                {
+                    // Trate qualquer exceção que possa ocorrer durante o processamento
+                    Console.WriteLine($"Erro ao processar o arquivo PDF: {ex.Message}");
+                    throw;
+                }
+
+                // Convertendo o MemoryStream para um array de bytes
+                pdfBytes = ms.ToArray();
+
+                SalvarPDF(pdfBytes, caminho_arquivo);
+                
+            }
+
+            return pdfBytes;
+        }
+
+        public void SalvarPDF(byte[] pdfBytes, string caminho_destino)
+        {
+            caminho_destino = Path.GetDirectoryName(caminho_destino);
+            // Salva o array de bytes como um arquivo PDF no caminho de destino
+            using (FileStream fs = new FileStream(caminho_destino, FileMode.Create))
+            {
+                fs.Write(pdfBytes, 0, pdfBytes.Length);
+            }
+        }
+
     }
 }
 
