149d5f7e34d7fb84ecedbb1aa4199185fa2b1a05
santosrs1 <Ramon.Santos-consultorexterno@rabobank.com>
2024-04-26 18:00:10 -0300

    Criado exemplo para chamar api

--- TesteGeracaoPDF/Controllers/PDFController.cs
+++ TesteGeracaoPDF/Controllers/PDFController.cs
@@ -43,46 +43,7 @@ namespace TesteGeracaoPDF.Controllers
             return File(pdfBytes, "application/pdf", "meu_arquivo.pdf");
         }
 
-        //[HttpGet("GetPDFDefault")]
-        //public string GetPDFDefault(string cliente, string data)
-        //{
-        //    try
-        //    {
-        //        Document doc = new Document(PageSize.A4);
-        //        doc.SetMargins(40, 40, 40, 80);
-        //        doc.AddCreationDate();
-
-        //        //caminho onde sera criado o pdf + nome desejado
-        //        //OBS: o nome sempre deve ser terminado com .pdf
-        //        string caminho = @"C:\temp\teste.pdf";
-
-        //        PdfWriter writer = PdfWriter.GetInstance(doc, new
-        //        FileStream(caminho, FileMode.Create));
-        //        doc.Open();
-
-        //        //criando uma string vazia
-        //        string dados = "";
-
-        //        //criando a variavel para paragrafo
-        //        Paragraph paragrafo = new Paragraph(dados,
-        //        new Font(Font.NORMAL, 14));
-               
-        //        paragrafo.Alignment = Element.ALIGN_JUSTIFIED;
-           
-        //        paragrafo.Add("TESTE TESTE TESTE");
-        //        //acidionado paragrafo ao documento
-        //        doc.Add(paragrafo);
-        //        //fechando documento para que seja salva as alteraçoes.
-        //        doc.Close();
-
-        //        return $"PDF GERADO COM SUCESSO EM {caminho}";
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return $"Erro na geração de pdf Erro: {ex.GetBaseException().Message}";
-        //    }
-            
-        //}
+       
 
       
     }
